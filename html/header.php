0d06c445220b1a3b97912f61e1ea6aeb117a5cbe
saturan <binder.lionel@gmail.com>
2015-08-04 11:07:34 +0200

    transformation en pages php
    rajout du head et du menu en include

---
+++ html/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Guerre et Chevalerie</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <link rel="stylesheet" href="../css/bootstrap-theme.css">
+  <link rel="stylesheet" href="../css/bootstrap.css">
+  <link href='http://fonts.googleapis.com/css?family=Tangerine' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+
+<header>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+          </button>
+          <a class="navbar-brand" href="index.html">Guerre et Chevalerie</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+             <li><a href="index.html">Accueil</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Nos Activités <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Spectacles équestres</a></li>
+                <li><a href="#">Fêtes médiévales</a></li>
+                <li><a href="#">Scolaires</a></li>
+                <li><a href="#">Centres aérés</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Médias <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Galeries photo / vidéos</a></li>
+                <li><a href="#">Liens</a></li>
+                <li><a href="#">Bétisier</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Nous contacter</a></li>
+                <li><a href="#">Nous rencontrer : notre calendrier</a></li>
+              </ul>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+  </header>
+
--- html/index.php
+++ html/index.php
@@ -10,49 +10,8 @@
 
 </head>
 <body>
-	<header>
-		<nav class="navbar navbar-default navbar-fixed-top">
-		  <div class="container-fluid">
-		    <!-- Brand and toggle get grouped for better mobile display -->
-		    <div class="navbar-header">
-		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-		        <span class="sr-only">Toggle navigation</span>
-		      </button>
-		      <a class="navbar-brand" href="index.html">Guerre et Chevalerie</a>
-		    </div>
 
-		    <!-- Collect the nav links, forms, and other content for toggling -->
-		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		      <ul class="nav navbar-nav navbar-right">
-						 <li><a href="index.html">Accueil</a></li>
-		        <li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Nos Activités <span class="caret"></span></a>
-		          <ul class="dropdown-menu">
-		            <li><a href="#">Spectacles équestres</a></li>
-		            <li><a href="#">Fêtes médiévales</a></li>
-		            <li><a href="#">Scolaires</a></li>
-		            <li><a href="#">Centres aérés</a></li>
-		          </ul>
-		        </li>
-		        <li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Médias <span class="caret"></span></a>
-		          <ul class="dropdown-menu">
-		            <li><a href="#">Galeries photo / vidéos</a></li>
-		            <li><a href="#">Liens</a></li>
-		            <li><a href="#">Bétisier</a></li>
-		          </ul>
-		        </li>
-		        <li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact <span class="caret"></span></a>
-		          <ul class="dropdown-menu">
-		            <li><a href="#">Nous contacter</a></li>
-		            <li><a href="#">Nous rencontrer : notre calendrier</a></li>
-							</ul>
-		      </ul>
-		    </div><!-- /.navbar-collapse -->
-		  </div><!-- /.container-fluid -->
-		</nav>
-	</header>
+	<?php include ('header.php') ?>
 
 	<!-- first section - Home -->
 	<div id="home" class="home">
